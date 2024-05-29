d316ec78e0477e2d03a968f92738fa5367f95eea
Ben Yip <ben_yip@126.com>
2024-05-29 23:46:01 +0800

    Add Header component

---
+++ src/components/header/Branding.tsx
@@ -0,0 +1,31 @@
+import { Typography } from "@mui/material";
+import ForumIcon from "@mui/icons-material/Forum";
+
+import React from "react";
+
+const Branding = () => {
+  return (
+    <>
+      <ForumIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 2,
+          display: { xs: "none", md: "flex" },
+          fontFamily: "monospace",
+          fontWeight: 700,
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        CHATTER
+      </Typography>
+    </>
+  );
+};
+
+export default Branding;
